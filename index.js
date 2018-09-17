6d318fd070171baed8f3d2de6cd9f1e2d6df77d1
Petya Djanovska <petya.djanovska@gmail.com>
2018-09-17 20:31:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function matchName(array, sample) {
   const newCollection = [];
 
   for (const user of array) {
-    if (findMatching(arra)) {
+    if (user.[name] === sample) {
       newCollection.push(user);
     }
   }
