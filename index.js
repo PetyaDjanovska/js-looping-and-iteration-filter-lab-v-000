81ffb176692da26b06002ddcc76dfa58e3a7db72
Petya Djanovska <petya.djanovska@gmail.com>
2018-09-17 18:39:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Code your solution in this file
+function findMatching(array, sample) {
+  const result = [];
+  result = array.filter(name => name.toLowerCase === sample.toLowerCase)) {
+  return result;
+  }
+}
